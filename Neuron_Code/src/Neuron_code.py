62dc843228eb6405dfaa6ab88950690efea7ca7b
Kea Maremela <keamogetsemaremela29@gmail.com.>
2026-03-12 06:45:26 +0200

    COMMIT A SIMPLE NEURON CODE

---
+++ Neuron_Code/src/Neuron_code.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# UNIQUE INPUTS
+# THREE INPUTS
+inputs  = [1.2, 5.1, 2.1]
+
+# WEIGHTSS
+# THREE INPUTS
+weights = [3.1, 2.1, 8.7]
+
+# BIAS 
+# EVERY UNIQUE NEURON HAS A UNIQUE BIAS
+bias = 3
+
+# OUTPUTS
+# OUTPUT = SUM-OF(INPUTS * WEIGHTS) + BIAS
+outputs =  inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+
+# PRINTING OUTPUTS
+print(f"outputs: {outputs}" )
+
+
+
